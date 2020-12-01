2243c33ec557a2313918c0a1254b912ec8afe5ad
spiritbro1 <rinoakbr@gmail.com>
2020-12-01 20:03:51 +0700

    database sudah diisi username kurang mengecek jika username sudah ada langsung login

--- functions/authorize.js
+++ functions/authorize.js
@@ -1,23 +1,34 @@
-'use strict';
-
-function github(params) {
-  return { payload: `The time in ${JSON.stringify(params.__bx_creds)}` };
-}
-
-module.exports.github = github;
-const main = function(params) { 
-
-  // configure the @cloudant/cloudant library
-  // const opts = {url: args.url, plugins: ['cookieauth', 'promises']};
-  const cloudant = require('@cloudant/cloudant')({url:params.__bx_creds.cloudantnosqldb.url,plugins: [ { iamauth: { iamApiKey: params.__bx_creds.cloudantnosqldb.apikey }}] });  
-  const db = cloudant.db.use("ecommerce");
+"use strict";
+const axios = require("axios");
+const formUrlEncoded = x =>
+   Object.keys(x).reduce((p, c) => p + `&${c}=${encodeURIComponent(x[c])}`, '')
+const github = async function (params) {
   
+  // if (params.code) {
+       
+  //   const { data } =await axios({
+  //     url: "https://github.com/login/oauth/access_token",
+  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded',"Accept": "application/json" },
+  //     data: formUrlEncoded({
+  //        client_id: params.github_client_id,
+  //        client_secret: params.github_client_secret,
+  //        code: params.code
+  //     }),
+  //     method:"POST"
+  //   });
+  //   return data;
+    
+  // } else {
+  //   return { payload: "error please ask backend" };
+  // }
+  const cloudant = require('@cloudant/cloudant')({url:params.__bx_creds.cloudantnosqldb.url,plugins: [ { iamauth: { iamApiKey: params.__bx_creds.cloudantnosqldb.apikey }}] });
+  const db = cloudant.db.use("ecommerce");
+
   // write a new document to the database
   const doc = {
-    timestamp: new Date().getTime(),
-    num: Math.random()
+    username: "spiritbro1"
   };
   return db.insert(doc);
 };
 
-module.exports.main = main;
+module.exports.github = github;
