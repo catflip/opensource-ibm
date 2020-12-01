8d7e5cfe132e5d1261645e2956a4a35bcde70ffe
spiritbro1 <rinoakbr@gmail.com>
2020-12-01 09:41:03 +0700

    berhasil cloudant thinggal github oauth dan save ke database cloudant

--- functions/authorize.js
+++ functions/authorize.js
@@ -1,8 +1,23 @@
 'use strict';
-const axios=require("axios")
-export default function github(params) {
-  const name = params.code || 'World';
-  return { payload: `Hello, ${name}!` };
+
+function github(params) {
+  return { payload: `The time in ${JSON.stringify(params.__bx_creds)}` };
 }
 
+module.exports.github = github;
+const main = function(params) { 
 
+  // configure the @cloudant/cloudant library
+  // const opts = {url: args.url, plugins: ['cookieauth', 'promises']};
+  const cloudant = require('@cloudant/cloudant')({url:params.__bx_creds.cloudantnosqldb.url,plugins: [ { iamauth: { iamApiKey: params.__bx_creds.cloudantnosqldb.apikey }}] });  
+  const db = cloudant.db.use("ecommerce");
+  
+  // write a new document to the database
+  const doc = {
+    timestamp: new Date().getTime(),
+    num: Math.random()
+  };
+  return db.insert(doc);
+};
+
+module.exports.main = main;
