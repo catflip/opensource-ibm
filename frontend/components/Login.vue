ec1bb3e8894824d1a3cefe60d5b05bf14edab9e8
spiritbro1 <rinoakbr@gmail.com>
2020-12-01 20:30:08 +0700

    kurang login ke github dan buat dashboard dan paypal

--- frontend/components/Login.vue
+++ frontend/components/Login.vue
@@ -5,7 +5,7 @@
   <v-card-text>
     <v-row :class="['my-3','mx-3','text-h4']">LOGIN USING YOUR GITHUB ACCOUNT</v-row>
     <v-row align="center" justify="center">
-      <v-btn><v-icon class="mx-2 my-2">fab fa-github</v-icon>LOGIN</v-btn>
+      <v-btn @click="login_github"><v-icon class="mx-2 my-2">fab fa-github</v-icon>LOGIN</v-btn>
     </v-row>
   </v-card-text>
 </v-card>
@@ -15,5 +15,9 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 @Component
-export default class MyStore extends Vue {}
+export default class MyStore extends Vue {
+  login_github(){
+    window.location.href="https://github.com/login/oauth/authorize?client_id="+process.env.github_client_id
+  }
+}
 </script>
